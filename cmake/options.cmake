fa34e92ba345136c6171af7d7ad9a47c50a1e26a
SaeHie Park <saehie.park@gmail.com>
2018-02-10 00:37:09 +0900

    Add FLAGS_CONLY

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -43,6 +43,11 @@ foreach(FLAG ${FLAGS_COMMON})
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${FLAG}")
 endforeach()
 
+# add c flags
+foreach(FLAG ${FLAGS_CONLY})
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${FLAG}")
+endforeach()
+
 # add cxx flags
 foreach(FLAG ${FLAGS_CXXONLY})
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${FLAG}")
